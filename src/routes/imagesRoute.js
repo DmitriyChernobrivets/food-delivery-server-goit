662aa58b2bdfd68b2a75af0a9f338d6ee8027d9a
DmitriyChernobrivects <mezete10@ukr.net>
2019-02-08 15:50:00 +0200

    fixed#1

--- src/routes/imagesRoute.js
+++ src/routes/imagesRoute.js
@@ -4,15 +4,15 @@ const imagesRoute = (req, res) => {
   if (req.files) {
     const resultSrc = req.files.reduce((acc, file) => {
       const { originalname } = file;
-      const desimationPath = "./src/db/images/" + originalname;
+      const dest = "./src/db/images/" + originalname;
 
-      fs.rename(file.path, desimationPath, function(err) {
+      fs.rename(file.path, dest, function(err) {
         if (err) throw err;
       });
 
-      return acc.concat(desimationPath);
+      return acc.concat(dest);
     }, []);
-    let response = responseBody("success", "imgsrr", resultSrc);
+    const response = responseBody("success", "imgsrr", resultSrc);
     res.writeHead(201, { "Content-Type": "application/json" });
     res.write(JSON.stringify(response));
     res.end();
